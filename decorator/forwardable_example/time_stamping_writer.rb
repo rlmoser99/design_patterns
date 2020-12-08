2f98d813e06f56c0892fbcdccb136f2651370743
Rachel Moser <moser.rachel@gmail.com>
2020-12-08 15:40:51 -0600

    forwardable example

---
+++ decorator/forwardable_example/time_stamping_writer.rb
@@ -0,0 +1,7 @@
+require_relative 'writer_decorator'
+
+class TimeStampingWriter < WriterDecorator
+  def write_line(line)
+    @real_writer.write_line("#{Time.new}: #{line}")
+  end
+end
---
+++ decorator/forwardable_example/writer_decorator.rb
@@ -0,0 +1,11 @@
+require 'forwardable'
+
+class WriterDecorator
+  extend Forwardable
+  
+  def_delegators :@real_writer, :write_line, :pos, :rewind, :close
+  
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+end
